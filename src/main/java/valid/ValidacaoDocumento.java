cf806adb0eafc7a656cb0a757a4d3609302d7e83
Daniel Diniz <daniel.diniz1@icloud.com>
2022-04-11 12:21:03 -0300

    utilizando biblioteca da caelum stella para validar o cpf

--- src/main/java/valid/ValidacaoDocumento.java
+++ src/main/java/valid/ValidacaoDocumento.java
@@ -1,4 +1,21 @@
 package valid;
 
+import br.com.caelum.stella.validation.CPFValidator;
+import br.com.caelum.stella.validation.InvalidStateException;
+
 public class ValidacaoDocumento {
+
+    public static void main(String[] args) {
+        String cpf = "86288366767";
+        CPFValidator cpfValidator = new CPFValidator();
+
+        try{
+            cpfValidator.assertValid(cpf);
+            System.out.println("CPF válido");
+
+        }catch (InvalidStateException ex){
+            System.out.println("CPF inválido: " + cpf + " é invalido! mensagem " + ex);
+        }
+
+    }
 }
