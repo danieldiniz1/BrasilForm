a4eb71929c854bb4109b1795592105d58a868f31
Daniel Diniz <daniel.diniz1@icloud.com>
2022-04-11 12:31:36 -0300

    refatorando e adicionando verificação para o CNPJ

--- src/main/java/valid/ValidacaoDocumento.java
+++ src/main/java/valid/ValidacaoDocumento.java
@@ -1,20 +1,35 @@
 package valid;
 
+import br.com.caelum.stella.tinytype.CNPJ;
+import br.com.caelum.stella.tinytype.CPF;
+import br.com.caelum.stella.validation.CNPJValidator;
 import br.com.caelum.stella.validation.CPFValidator;
 import br.com.caelum.stella.validation.InvalidStateException;
 
 public class ValidacaoDocumento {
 
     public static void main(String[] args) {
-        String cpf = "86288366767";
+        // Validação de CPF
+        CPF cpf = new CPF("86288366767");
         CPFValidator cpfValidator = new CPFValidator();
 
         try{
-            cpfValidator.assertValid(cpf);
+            cpfValidator.assertValid(cpf.getNumero());
             System.out.println("CPF válido");
 
         }catch (InvalidStateException ex){
-            System.out.println("CPF inválido: " + cpf + " é invalido! mensagem " + ex);
+            System.out.println("CPF inválido: " + cpf.getNumeroFormatado() + ". ERRO: " + ex);
+        }
+
+        //validação de CNPJ
+        CNPJ cnpj = new CNPJ("78.284.376/0001-62");
+        CNPJValidator cnpjValidator = new CNPJValidator();
+        try{
+            cnpjValidator.assertValid(cnpj.getNumero());
+            System.out.println("CNPJ válido");
+
+        }catch (InvalidStateException ex){
+            System.out.println("CNPJ inválido: " + cnpj.getNumeroFormatado() + ". ERRO: " + ex);
         }
 
     }
