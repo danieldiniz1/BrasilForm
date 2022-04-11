05f406cc9fb7a82506b1c90118dc0bb25d1229e0
Daniel Diniz <daniel.diniz1@icloud.com>
2022-04-11 18:37:19 -0300

    adiciconando conversor de moeda

--- src/main/java/valid/Dinheiro.java
+++ src/main/java/valid/Dinheiro.java
@@ -1,21 +1,51 @@
 package valid;
 
+import org.javamoney.moneta.FastMoney;
 import org.javamoney.moneta.Money;
+import org.javamoney.moneta.convert.ExchangeRateType;
 
 import javax.money.CurrencyUnit;
 import javax.money.Monetary;
 import javax.money.MonetaryAmount;
+import javax.money.convert.CurrencyConversion;
+import javax.money.convert.ExchangeRateProvider;
+import javax.money.convert.MonetaryConversions;
+import java.math.BigDecimal;
+import java.util.List;
 
 public class Dinheiro {
 
     public static void main(String[] args) {
-        CurrencyUnit moeda = Monetary.getCurrency("BRL");
-        MonetaryAmount valorDaParcela = Money.of(75,moeda);
+        CurrencyUnit real = Monetary.getCurrency("BRL");
+        CurrencyUnit dolar = Monetary.getCurrency("USD");
+        MonetaryAmount valorDaParcela = Money.of(75,real);
         System.out.println(valorDaParcela);
 
         MonetaryAmount valorTotal = valorDaParcela.multiply(12);
         System.out.println(valorTotal.getNumber());
+
+        MonetaryAmount valorProdutoDolar = FastMoney.of(90, dolar);
+        MonetaryAmount valorImpostoReal = FastMoney.of(30, real);
+
+        MonetaryAmount impostoConvertido = conversorDeValor(BigDecimal.valueOf(30), real.getCurrencyCode(), dolar.getCurrencyCode());
+        System.out.println(valorProdutoDolar.add(impostoConvertido));
     }
 
+    private static MonetaryAmount conversorDeValor(BigDecimal valor, String moeda1, String moeda2){
+        // pegar o valor e as moedas para conversao;
+        CurrencyUnit moedaLocal = Monetary.getCurrency(moeda1);
+        CurrencyUnit moedaConversao = Monetary.getCurrency(moeda2);
+        MonetaryAmount valorDaConversao = FastMoney.of(valor,moedaLocal);
+
+        //Instancia do provedor de informações de rate;
+        ExchangeRateProvider exchangeRateProvider = MonetaryConversions.getExchangeRateProvider(ExchangeRateType.ECB);
+
+        //Pegar a conversão do dia;
+        CurrencyConversion currencyConversion = exchangeRateProvider.getCurrencyConversion(moedaConversao);
+
+        //Transformando o valor do imposto com o rate atual e devolvendo para o método o resultado;
+        MonetaryAmount valorAtualizado = currencyConversion.apply(valorDaConversao);
+        return valorAtualizado;
+    }
 
 }
