774e61e424d4980328179701e54d805cb448e76f
Daniel Diniz <daniel.diniz1@icloud.com>
2022-04-11 12:58:16 -0300

    refatoring

--- src/main/java/valid/ValidacaoDocumento.java
+++ src/main/java/valid/ValidacaoDocumento.java
@@ -1,20 +1,18 @@
 package valid;
 
+import br.com.caelum.stella.format.TituloEleitoralFormatter;
 import br.com.caelum.stella.tinytype.CNPJ;
 import br.com.caelum.stella.tinytype.CPF;
-import br.com.caelum.stella.validation.CNPJValidator;
-import br.com.caelum.stella.validation.CPFValidator;
-import br.com.caelum.stella.validation.InvalidStateException;
+import br.com.caelum.stella.validation.*;
 
 public class ValidacaoDocumento {
 
     public static void main(String[] args) {
         // Validação de CPF
-        CPF cpf = new CPF("86288366767");
-        CPFValidator cpfValidator = new CPFValidator();
+        CPF cpf = new CPF("86288366757");
 
         try{
-            cpfValidator.assertValid(cpf.getNumero());
+            validarDocumentos(new CPFValidator(), cpf.getNumero());
             System.out.println("CPF válido");
 
         }catch (InvalidStateException ex){
@@ -23,14 +21,28 @@ public class ValidacaoDocumento {
 
         //validação de CNPJ
         CNPJ cnpj = new CNPJ("78.284.376/0001-62");
-        CNPJValidator cnpjValidator = new CNPJValidator();
         try{
-            cnpjValidator.assertValid(cnpj.getNumero());
+            validarDocumentos(new CNPJValidator(),cnpj.getNumero());
             System.out.println("CNPJ válido");
 
         }catch (InvalidStateException ex){
             System.out.println("CNPJ inválido: " + cnpj.getNumeroFormatado() + ". ERRO: " + ex);
         }
 
+        //validação de Título de eleitor
+        String tituloDeEleitor = "146512320140";
+
+        try{
+            validarDocumentos(new TituloEleitoralValidator(),tituloDeEleitor);
+            System.out.println("Titulo de eleitor válido");
+
+        }catch (InvalidStateException ex){
+            System.out.println("Titulo de eleitor inválido: " + tituloDeEleitor + ". ERRO: " + ex);
+        }
+
+    }
+
+    private static void validarDocumentos(Validator<String> validator, String documento){
+        validator.assertValid(documento);
     }
 }
